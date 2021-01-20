801a8a74dddd52159997adfd8b1346e5954631db
Caio Stoduto <35301126+CaioStoduto@users.noreply.github.com>
2021-01-20 09:12:37 -0300

    docs: just adding documentation

--- src/routes/oauth.js
+++ src/routes/oauth.js
@@ -2,13 +2,18 @@
 const router = require('express').Router()
 const OAuth = require('../util/oauth')
 
+//* Gets all the OAuth methods avaliable
 Object.keys(OAuth).forEach((key) => {
+  //* Sets the router to accept [GET] requests of each OAuth Service
   router.get(`/${key}`, async (req, res) => {
+    //* Tries to get the identifier with the redirect parameters
     const loginID = await OAuth[key].getLoginIdentifier(req.query)
 
+    //* If it successfully gets the ID
     if (loginID.err === undefined && loginID.identifier) {
       return res.status(200).send(`Your ID on ${key} is ${loginID.identifier}`).end()
     } else {
+      //* Shows the error that occured
       return res.status(loginID.err.code).send(`${loginID.err.message}\n\nSomething went wrong, try again later, http://${req.get('host')}`).end()
     }
   })
--- src/util/oauth.js
+++ src/util/oauth.js
@@ -1,45 +1,61 @@
+//* Importing dependencies
 const axios = require('axios')
 
+//* Exporting all OAuth methods to able others modules to use it
 module.exports = {
   GitHub: {
+    //* Function to get the URL used to redirect the user to the OAuth
     authURL: (host) => {
+      //* Sets the URL to turn it easier to add parameters
       const url = new URL('https://github.com/login/oauth/authorize')
       const searchParams = url.searchParams
 
+      //* Adds the parameters to the url.searchParams object
       searchParams.append('client_id', process.env.GITHUB_PUBLIC)
       searchParams.append('redirect_uri', `http://${host}/OAuth/GitHub`)
 
+      //* Sets the (String) url.searchParams to url.search to maintain consistency
       url.search = searchParams.toString()
 
+      //* Returns the URL ready to be used
       return url.toString()
     },
+    //* Function to get the identifier of the OAuth service by the response of the OAuth
     getLoginIdentifier: async (query) => {
+      //* If the query (the request parameters) has the parameter code
       if (query.code) {
+        //* Requests to the OAuth Service the access_token with the code provided by the OAuth redirect
         const res = await axios.post('https://github.com/login/oauth/access_token', '', {
+          //* Sets the parameters required to request the access_token
           params: {
             client_id: process.env.GITHUB_PUBLIC,
             client_secret: process.env.GITHUB_SECRET,
             code: query.code
           },
+          //* Header set to get the response in json
           headers: {
             Accept: 'application/json'
           }
         })
 
+        //* If occured any error, it will return the error
         if (res.data.error) {
           return { err: { code: 400, message: res.data.error_description } }
         }
 
+        //* Requests to the OAuth Service the user identified with the access_code
         const res2 = await axios.get('https://api.github.com/user', {
+          //* Sets the header required to request the ID
           headers: {
             Authorization: `token ${res.data.access_token}`
           }
-        }).catch((error) => {
+        }).catch((error) => { //* In case that occurs any error, it will return the error
           return { err: { code: error.response.status, message: error.response.data } }
         })
 
+        //* Returns the identifier that was given by the OAuth Service
         return { identifier: res2.data.id }
-      } else {
+      } else { //* Occured an error, so it will return the error
         return { err: { code: 400, message: 'Code parameter missing.' } }
       }
     }
