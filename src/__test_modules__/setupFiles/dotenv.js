347f3faa040ce7149cee3a7c86b2a60b202e431b
Caio Stoduto <35301126+CaioStoduto@users.noreply.github.com>
2021-01-20 13:09:20 -0300

    feat: improving tests

--- src/__test_modules__/setupFiles/dotenv.js
+++ src/__test_modules__/setupFiles/dotenv.js
@@ -1,4 +1,28 @@
+/** Environment Variables
+ * DB_AUTH (mongodb+srv://{USER}:{DB_PASSWORD}@{DB_HOST})
+ * DB_NAME
+ * GITHUB_PUBLIC
+ * GITHUB_SECRET
+ */
+
 //* Importing and applying dotenv (a module that loads environment variables from a .env.test file into process.env)
 require('dotenv').config({
   path: require('path').resolve(__dirname, '../../../.env.test')
 })
+
+//* Setting a global function that will help in tests (https://stackoverflow.com/questions/1497481/javascript-password-generator)
+global.randomString = (length) => {
+  const charset = '@#$%{}[]()\\/\'"`~,;:.<>abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'
+  const n = charset.length
+  let string
+
+  for (let i = 0; i < length; ++i) {
+    string += charset.charAt(Math.floor(Math.random() * n))
+  }
+
+  return string
+}
+
+//* Generates those secrets every test to prove that the tests are correct
+process.env.COOKIES_SECRET = global.randomString(64)
+process.env.JWT_SECRET = global.randomString(64)
