15c4fff35bdfba491392306d0cf47c9ee557313c
Caio Stoduto <35301126+CaioStoduto@users.noreply.github.com>
2021-01-27 12:26:11 -0300

    test: adding all tests of [GET] /tools

--- src/routes/__tests__/tools.test.js
+++ src/routes/__tests__/tools.test.js
@@ -1,10 +1,13 @@
 //* Importing dependencies
-const jwt = require('jsonwebtoken')
+// Importing and applying .src/server to test .src/routes/tools
+const request = require('supertest')(global.app)
 
 const { randomUser, signCookie } = global.misc
+const jTool = global.joi.Tool
 const { Tool } = global.models
 
 const defaultTools = require('../../extras/defaultTools.json')
+const jwt = require('jsonwebtoken')
 
 //* Init Database
 global.misc.setupDB(false)
@@ -29,7 +32,7 @@ afterEach(async () => {
 
 describe('Authentication Middleware', () => {
   //* Using a unused route to test authentication
-  const _app = global.app
+  const _app = global.app // Using personalized app to test specifically authentication
   _app.put('/tools', (req, res, next) => {
     return res.status(200).send('OK').end()
   })
@@ -51,12 +54,10 @@ describe('Authentication Middleware', () => {
   })
 
   test('User.authorized === false', async () => {
-    const user = await randomUser({ authorized: false })
-
     //* Simulates the user request
     const result = await _request
       .put('/tools')
-      .set('Cookie', [`auth-token=${signCookie(jwt.sign({ _id: user._id }, process.env.JWT_SECRET, { expiresIn: '2d' }), process.env.COOKIES_SECRET)}`])
+      .set('Cookie', [`auth-token=${signCookie(jwt.sign({ _id: (await randomUser({ authorized: false }))._id }, process.env.JWT_SECRET, { expiresIn: '2d' }), process.env.COOKIES_SECRET)}`])
 
     //* Throws if an error occured
     if (result.err) throw result.err
@@ -64,5 +65,111 @@ describe('Authentication Middleware', () => {
     //* Tests the result to proof if it worked properly
     expect(result.status).toBe(401)
     expect(result.text).toEqual('You\'re logged in, but you aren\'t authorized.')
+  })
+
+  test('User', async () => {
+    //* Simulates the user request
+    const result = await _request
+      .put('/tools')
+      .set('Cookie', [`auth-token=${signCookie(jwt.sign({ _id: (await randomUser())._id }, process.env.JWT_SECRET, { expiresIn: '2d' }), process.env.COOKIES_SECRET)}`])
+
+    //* Throws if an error occured
+    if (result.err) throw result.err
+
+    //* Tests the result to proof if it worked properly
+    expect(result.status).toBe(200)
+    expect(result.text).toEqual('OK')
+  })
+})
+
+describe('GET /tools', () => {
+  test('No parameters', async () => {
+    //* Simulates the user request
+    const result = await request
+      .get('/tools')
+      .set('Cookie', `auth-token=${signCookie(jwt.sign({ _id: (await randomUser())._id }, process.env.JWT_SECRET, { expiresIn: '2d' }), process.env.COOKIES_SECRET)}`)
+
+    const tools = []
+
+    JSON.parse(result.text).forEach((tool) => {
+      const { error, value } = jTool.validate(tool)
+      if (error) throw error
+      tools.push(value)
+    })
+
+    expect(result.status).toEqual(200)
+    expect(tools).toEqual(defaultTools)
+  })
+
+  test('?tag=', async () => {
+    //* Simulates the user request
+    const result = await request
+      .get('/tools?tag=')
+      .set('Cookie', `auth-token=${signCookie(jwt.sign({ _id: (await randomUser())._id }, process.env.JWT_SECRET, { expiresIn: '2d' }), process.env.COOKIES_SECRET)}`)
+
+    const tools = []
+
+    JSON.parse(result.text).forEach((tool) => {
+      const { error, value } = jTool.validate(tool)
+      if (error) throw error
+      tools.push(value)
+    })
+
+    expect(result.status).toEqual(200)
+    expect(tools).toEqual(defaultTools)
+  })
+
+  test('?tag=a', async () => {
+    //* Simulates the user request
+    const result = await request
+      .get('/tools?tag=a')
+      .set('Cookie', `auth-token=${signCookie(jwt.sign({ _id: (await randomUser())._id }, process.env.JWT_SECRET, { expiresIn: '2d' }), process.env.COOKIES_SECRET)}`)
+
+    const tools = []
+
+    JSON.parse(result.text).forEach((tool) => {
+      const { error, value } = jTool.validate(tool)
+      if (error) throw error
+      tools.push(value)
+    })
+
+    expect(result.status).toEqual(200)
+    expect(tools).toEqual([])
+  })
+
+  test('?tag=node', async () => {
+    //* Simulates the user request
+    const result = await request
+      .get('/tools?tag=node')
+      .set('Cookie', `auth-token=${signCookie(jwt.sign({ _id: (await randomUser())._id }, process.env.JWT_SECRET, { expiresIn: '2d' }), process.env.COOKIES_SECRET)}`)
+
+    const tools = []
+
+    JSON.parse(result.text).forEach((tool) => {
+      const { error, value } = jTool.validate(tool)
+      if (error) throw error
+      tools.push(value)
+    })
+
+    expect(result.status).toEqual(200)
+    expect(tools).toEqual(defaultTools.filter(({ tags }) => tags.includes('node')))
+  })
+
+  test('?tag=calendar', async () => {
+    //* Simulates the user request
+    const result = await request
+      .get('/tools?tag=calendar')
+      .set('Cookie', `auth-token=${signCookie(jwt.sign({ _id: (await randomUser())._id }, process.env.JWT_SECRET, { expiresIn: '2d' }), process.env.COOKIES_SECRET)}`)
+
+    const tools = []
+
+    JSON.parse(result.text).forEach((tool) => {
+      const { error, value } = jTool.validate(tool)
+      if (error) throw error
+      tools.push(value)
+    })
+
+    expect(result.status).toEqual(200)
+    expect(tools).toEqual(defaultTools.filter(({ tags }) => tags.includes('calendar')))
   })
 })
